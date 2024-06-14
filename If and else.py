b454e24c00266b94be85325f4e62fff0782402d3
MrGrimsz <159098065+MrGrimsz@users.noreply.github.com>
2024-06-14 11:33:51 -0500

    Add files via upload

---
+++ If and else.py
@@ -0,0 +1,39 @@
+#condition 2
+
+#name = "Jorge"
+#if name in ["Reeni","Jorge"]:
+#    print("I SEE YOU")
+'''''
+Yes = True
+No = False
+if Yes is True:
+    print("GO on")
+elif No is True:
+    print("Stop CUNT")
+else:
+    print("gUAT???")
+
+
+x = 2
+if x == 3:
+    print("Correct")
+else:
+    print("No wrong")
+
+x = [1,2,3]
+y = [1,2,3]
+print(x == y) #Prints out true
+print(x is y) #Prints out false
+
+print(not False)
+print((False) == (False))
+'''
+
+lava = 3
+
+if lava == 1:
+    print("wow")
+elif lava == 2:
+    print('ye')
+else:
+    print("Guat")
