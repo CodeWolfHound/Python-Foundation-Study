f1467113cbf3841227e784eeb68fde17066026fe
MrGrimsz <159098065+MrGrimsz@users.noreply.github.com>
2024-06-14 11:24:16 -0500

    Add files via upload
    Variable Conditions Test

---
+++ # Conditons.py
@@ -0,0 +1,16 @@
+# Conditons
+x = 2
+print(x == 2)
+print(x == 3)
+print(x < 3)
+
+#Name Test
+name = "Ron"
+name = "Alan"
+Age = 25
+if name == "Alan" and Age == 25:
+        print(" Dam you 25??")
+
+if name == "Alan" or name == "Don":
+                print("You not alanis ???")
+
